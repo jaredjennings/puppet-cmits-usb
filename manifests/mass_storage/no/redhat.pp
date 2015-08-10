c236bded9b1436d0a1681721dbf02027e4653e0d
Jared Jennings <jjennings@fastmail.fm>
2015-08-10 17:06:54 -0500

    take care of case and quoting in osfamily selectors; get rid of unimplemented() uses (Puppet 4 compatibility)

--- manifests/mass_storage/no/redhat.pp
+++ manifests/mass_storage/no/redhat.pp
@@ -14,5 +14,5 @@
 # % See the License for the specific language governing permissions and
 # % limitations under the License.
 class usb::mass_storage::no::redhat {
-    unimplemented()
+    fail "unimplemented"
 }
